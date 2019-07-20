8dfd6cabbe6cb6cc72435402a1167db8d207c384
igatatsu <45650088+igatatsu@users.noreply.github.com>
2019-07-20 19:29:41 +0900

    Update index.js

--- public/js_client/index.js
+++ public/js_client/index.js
@@ -1,60 +1,20 @@
 "use strict";
-//document.onkeydown = GetKeyCode;
-//document.onkeyup = OffKeyCode;
+document.onkeydown = keydown;
 document.onkeyup = keyup;
-let KeyState = new Array();
+let Keystate = new Array();
 let c = 0;  //counter
 let canvas;
-let canAdd = true;
+//let canAdd = true;
 let url;
 let name;
+let cid;  //command id
 let prop = {
   id: "",
-  command: [],
-  hitEvent: [],
-  color: ""
+  command: [],  //First phase commands
+  hitEvent: [], //second phase commands
+  color: "",
+  mode:0
 };
-
-
-
-/*
-function GetKeyCode(event){
-  switch(event.key){
-    case 'a':
-      KeyState[0] = true;
-      break;
-    case 'b':
-      KeyState[1] = true;
-      break;
-    case 'e':
-      KeyState[2] = true;
-      break;
-  }
-}
-
-function OffKeyCode(event){
-  input();
-  switch(event.key){
-    case 'a':
-      KeyState[0] = false;
-      break;
-    case 'b':
-      KeyState[1] = false;
-      break;
-    case 'e':
-      KeyState[2] = false;
-      break;
-  }
-}
-
-
-function input(){
-  if(KeyState[0] == true && KeyState[1] == true){
-    console.log("true");
-  }
-}
-
-*/
 
 function getButton(discriminate) {
   if ("go" in discriminate) {
@@ -89,135 +49,216 @@ function getButton(discriminate) {
   }
 }
 
+function keydown(event){
+  event_block:{
+    switch(event.key){
+      case '1':
+        Keystate[0] = true;
+        break;
+      case '2':
+        Keystate[1] = true;
+        break;
+      case '3':
+        Keystate[2] = true;
+        break;
+      case '4':
+        Keystate[3] = true;
+        break;
+      case 'r':
+        cid = "reset";
+        break;
+      case 's':
+        cid = "send";
+        break;
+      case 't':
+        cid = "subsend";
+        break;
+      case 'd':
+        cid = "onereturn";
+        break;
+      default:
+        cid = undefined;
+    }
+
+    if(Keystate[0] == true){
+      if(Keystate[1] == true) cid = "right15";
+      else if(Keystate[3] == true) cid = "left15";
+      else cid = "go";
+    }
+    else if(Keystate[2] == true){
+      if(Keystate[1] == true) cid = "right135";
+      else if(Keystate[3] == true) cid = "left135";
+      else cid = "back";
+    }
+    else if(Keystate[1] == true) cid = "right90";
+    else if(Keystate[3] == true) cid = "left90";
+
+    if(cid == undefined) break event_block;
+
+    let pressing = function(){
+      if(c%2 == 0 || event.key == 'r' || event.key == 's') $("#"+cid).addClass("pressing");
+      else $("#"+cid+"Final").addClass("pressing");
+    }
+
+    setTimeout(pressing,50)
+  }
+}
+
 function keyup(event){
-  if (!url.match("color")) alert("色を指定してください");
+  event_block:{
+    if(!url.match("color")) {
+      alert("色を指定してください");
+      break event_block;
+    }
 
-  let command;
-  switch(event.key){
-    case '1':
-      command = {roll: -15};
-      break;
-    case '2':
-      command = {roll: -90};
-      break;
-    case '3':
-      command = {roll: -135};
-      break;
-    case '4':
-      command = {go: 15};
-      break;
-    case '5':
-      command = {roll: 15};
-      break;
-    case '6':
-      command = {roll: 90};
-      break;
-    case '7':
-      command = {roll: 135};
-      break;
-    case '8':
-      command = {roll: 180};
-      break;
-    case 'r':
-      name.value = '';
-      prop.id = "";
-      prop.command = [];
-      prop.hitEvent = [];
-      addCommand();
-      addEvent();
-      break;
-    case 's':
-      if (!canAdd) return;
-      canAdd = false;
-      setTimeout(function () {
-        return canAdd = true;
-      }, 5000);
-      send('message');
-      break;
-    case 't': //demo
-      send('demo' + socket.id);
-      break;
-    case 'c':
-      c++;
-      break;
-  }
+    let command;
 
-  if(event.key != 'c' && c%2 == 0){
-    if(event.key == 'd'){
-      prop.command.pop();
-      addCommand();
+    if(Keystate[0] == true){
+      if(Keystate[1] == true) command = {roll: 15};
+      else if(Keystate[3] == true) command = {roll: -15};
+      else command = {go: 15};
     }
-    else if(event.key != ('r' || 's' || 't')){
-      prop.command.push(command);
-      addCommand();
+    else if(Keystate[2] == true){
+      if(Keystate[1] == true) command = {roll: 135};
+      else if(Keystate[3] == true) command = {roll: -135};
+    else command = {roll: 180};
     }
-  }
-  else if(event.key != 'c'){
-    if(event.key == 'd'){
-      prop.hitEvent.pop();
-      addEvent();
+    else if(Keystate[1] == true) command = {roll: 90};
+    else if(Keystate[3] == true) command = {roll: -90};
+
+    for(let i=0;i<4;i++) Keystate[i] = false;
+
+    switch(event.key){
+      case 'r':
+        reset();
+        break;
+      case 's':
+        /*if (!canAdd) return;
+        canAdd = false;
+        setTimeout(function () {
+          return canAdd = true;
+        }, 5000);*/
+        $("#"+cid).removeClass("pressing");
+        send('message');
+        break;
+      case 't': //demo
+        if(c%2==0) $("#"+cid).removeClass("pressing");
+        else $("#"+cid+"Final").removeClass("pressing");
+        send('demo' + socket.id);
+        break;
+      case 'c':
+        c++;
+        active();
+        break;
+      case 'd':
+        command = "delete";
+        break;
     }
-    else if(event.key != ('r' || 's' || 't')){
-      prop.hitEvent.push(command);
-      addEvent();
+
+    if(command == undefined) break event_block;
+
+    if(c%2 == 0){
+      $("#"+cid).removeClass("pressing");
+
+      if(event.key == 'd'){
+        prop.command.pop();
+        addFPC();
+      }
+      else {
+        prop.command.push(command);
+        addFPC();
+      }
+    }
+    else{
+      $("#"+cid+"Final").removeClass("pressing");
+
+      if(event.key == 'd'){
+        prop.hitEvent.pop();
+        addSPC();
+      }
+      else{
+        prop.hitEvent.push(command);
+        addSPC();
+      }
     }
   }
 }
 
+function createBlock(className){
+  return function(command){
+    let el = document.createElement("div");
+    el.className = className;
+    el.style.marginBottom = "20px";
+    el.innerHTML = getButton(command);
+    return el;
+  };
+}
 
-/*
-$(function(){
-  $(".phase").click(function(){
-    if(($this).hasClass("active")) $(this).removeClass("active");
-    else if(!$(this).hasClass("active")) $(this).addClass("active");
-  });
-});
-*/
-
-function addElement() {
+function addElement(id, commands, className){
   console.log(prop);
+  let list = document.getElementById(id);
+  list.innerHTML = "";
+  commands.map(createBlock(className)).forEach(function(el){
+    return list.appendChild(el);
+  });
+  list.scrollTop = list.scrollHeight;
 }
 
-function addCommand() {
-  addElement();
-  let commandList = document.getElementById('messageList');
-  commandList.innerHTML = "";
-
-  for (let i = 0; i < prop.command.length; i++) {
-    let command = document.createElement("div");
-    command.className = "block1";
-    command.style.marginBottom = "20px";
-    command.innerHTML = getButton(prop.command[i]);
-    commandList.appendChild(command);
-    commandList.scrollTop = commandList.scrollHeight;
-  }
+function addFPC() {
+  addElement('FPCList', prop.command, "block1");
 }
 
-function addEvent() {
-  addElement();
-  let hitEventList = document.getElementById('hitEventList');
-  hitEventList.innerHTML = "";
-
-  for (let i = 0; i < prop.hitEvent.length; i++) {
-    let hitEvent = document.createElement("div");
-    hitEvent.className = "block2";
-    hitEvent.style.marginBottom = "20px";
-    hitEvent.innerHTML = getButton(prop.hitEvent[i]);
-    hitEventList.appendChild(hitEvent);
-    hitEventList.scrollTop = hitEventList.scrollHeight;
-  }
+function addSPC() {
+  addElement('SPCList', prop.hitEvent, "block2");
 }
 
 function send(id) {
   prop.id = name.value;
 
-  if (prop.command.length === 0 || prop.id === "") {
-    alert('入力されていない部分があります');
+  if (prop.command.length === 0 || prop.hitEvent.length === 0 || prop.id === "") {
+    alert("入力されていない部分があります");
     return false;
   }
 
-  console.log(prop);
-  socket.emit(id, JSON.stringify(prop));
+  if(id != "message"){
+    console.log(prop);
+    socket.emit(id, JSON.stringify(prop));
+  }
+  else if(window.confirm("アップロードしてもよろしいですか？")){
+    alert("アップロードしました。");
+    console.log(prop);
+    socket.emit(id, JSON.stringify(prop));
+  }
+  else alert("アップロードをやめました。");
+}
+
+function active(){
+  if(c%2 == 0) {
+    $("#first").addClass("active");
+    $("#second").removeClass("active");
+    prop.mode=0;
+  }
+  else{
+    $("#second").addClass("active");
+    $("#first").removeClass("active");
+    prop.mode=1;
+  }
+}
+
+function reset(){
+  name.value = '';
+  prop.id = "";
+  prop.command = [];
+  prop.hitEvent = [];
+  addFPC();
+  addSPC();
+  c=0;
+  prop.mode=0;
+  if($("#second").hasClass("active")) {
+    $("#second").removeClass("active");
+    $("#first").addClass("active");
+  }
+  $("#"+cid).removeClass("pressing");
 }
 
 window.onload = function () {
